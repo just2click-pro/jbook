424e501a5d875afda3fb723ec915caaa881ccfd0
Dror Avidov <just2click@gmail.com>
2023-02-06 09:32:55 +0200

    [main] Make the code run

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const ref = useRef<any>()
+  const iframeRef = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -35,9 +36,30 @@ const App = () => {
       }
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   };
 
+  const html = `
+    <html>
+      <head>
+
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (e) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: tomato;"><h4>Runtime Error</h4>' + err + '</div>';
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `
   return (
     <div>
       <textarea
@@ -48,7 +70,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="" src="/test.html"/>
+      <iframe ref={iframeRef} sandbox="allow-scripts" srcDoc={html}/>
     </div>
   )
 }
